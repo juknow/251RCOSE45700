7d9ba12d5ad7f89182017e019efe5cd0b5eadf62
juknow <munjunho77@naver.com>
2025-06-03 21:40:51 +0900

    [feat] 네트워크 다시 추가

---
+++ Assets/Scripts/Manager/NetworkLobbyManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using Mirror;
+using UnityEngine.UI;
+using TMPro;
+
+public class NetworkLobbyManager : NetworkManager
+{
+    [Header("Lobby UI")]
+    public Button hostButton;
+    public Button joinButton;
+    public TMP_InputField ipInputField;
+    public GameObject readyButton;
+    public TextMeshProUGUI statusText;
+
+    private void Start()
+    {
+        // UI 버튼 이벤트 연결
+        hostButton.onClick.AddListener(OnHostButtonClicked);
+        joinButton.onClick.AddListener(OnJoinButtonClicked);
+    }
+
+    public void OnHostButtonClicked()
+    {
+        StartHost();
+        statusText.text = "호스트로 시작됨";
+        readyButton.SetActive(true);
+    }
+
+    public void OnJoinButtonClicked()
+    {
+        networkAddress = ipInputField.text;
+        StartClient();
+        statusText.text = "참가자로 시작됨";
+        readyButton.SetActive(true);
+    }
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        Debug.Log("서버가 시작되었습니다.");
+    }
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+        Debug.Log("클라이언트가 시작되었습니다.");
+    }
+}
---
+++ Assets/Scripts/Player/NetworkPlayer.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class NetworkPlayer : NetworkBehaviour
+{
+    [SyncVar]
+    private bool isReady = false;
+
+    [SerializeField]
+    private GameObject playerPrefab;
+
+    public void SetReady()
+    {
+        if (!isReady)
+        {
+            CmdSetReady();
+        }
+    }
+
+    [Command]
+    private void CmdSetReady()
+    {
+        isReady = true;
+        RpcUpdateReadyStatus(isReady);
+        CheckAllPlayersReady();
+    }
+
+    [ClientRpc]
+    private void RpcUpdateReadyStatus(bool ready)
+    {
+        isReady = ready;
+    }
+
+    [Server]
+    private void CheckAllPlayersReady()
+    {
+        NetworkPlayer[] players = FindObjectsOfType<NetworkPlayer>();
+        bool allReady = true;
+
+        foreach (NetworkPlayer player in players)
+        {
+            if (!player.isReady)
+            {
+                allReady = false;
+                break;
+            }
+        }
+
+        if (allReady && players.Length >= 2)
+        {
+            // 게임 씬으로 전환
+            NetworkManager.singleton.ServerChangeScene("GameScene");
+        }
+    }
+}

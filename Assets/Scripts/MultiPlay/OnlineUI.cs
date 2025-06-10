67b68bc8e38b1ccaf43bda971f963dba92061e58
juknow <munjunho77@naver.com>
2025-06-10 21:22:32 +0900

    [feat] 네트워크 클라이언트 생성

---
+++ Assets/Scripts/MultiPlay/OnlineUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using Mirror;
+public class OnlineUI : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+    public void OnClickStartServer()
+    {
+        var manager = RoomManager.singleton;
+        manager.StartClient();
+    }
+}
--- Assets/Scripts/MultiPlay/RoomManager.cs
+++ Assets/Scripts/MultiPlay/RoomManager.cs
@@ -5,4 +5,10 @@ using Mirror;
 
 public class RoomManager : NetworkRoomManager
 {
+    public override void OnRoomServerConnect(NetworkConnectionToClient conn)
+    {
+        base.OnRoomServerConnect(conn);
+        var player = Instantiate(spawnPrefabs[0]);
+        NetworkServer.Spawn(player, conn);
+    }
 }
